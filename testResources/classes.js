820654bde8a4a35392a00c29be66854af62843e3
Nico Jansen <jansennico@gmail.com>
2020-02-14 14:48:39 +0100

    build(eslint-prettier): migrate tslint -> eslint&prettier

--- testResources/classes.js
+++ testResources/classes.js
@@ -1,21 +1,20 @@
 'use strict';
 
 class ESPerson {
-    constructor(name, age) {
-        this.name = name;
-        this.age = 42;
-    }
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 }
 
 function PrototypePerson(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
-class ConstructorLessClass {
-}
+class ConstructorLessClass {}
 
 exports.ESPerson = ESPerson;
 exports.PrototypePerson = PrototypePerson;
 exports.ConstructorLessClass = ConstructorLessClass;
-exports.NamelessClass = class { };
+exports.NamelessClass = class {};
